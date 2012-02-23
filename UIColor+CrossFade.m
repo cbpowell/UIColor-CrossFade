237f1686a845d1a94b644c3c8daa50b2f40f9503
Peyton Randolph <pjrandolph@gmail.com>
2012-02-23 11:39:20 -0500

    Add colorspace conversion

--- UIColor+CrossFade.m
+++ UIColor+CrossFade.m
@@ -35,12 +35,11 @@
                                secondColor:(UIColor *)secondColor 
                                    atRatio:(CGFloat)ratio {
     
-    // If the UIColor arguments lie in different color spaces, return nil
-    // todo: convert between color spaces (http://arstechnica.com/apple/guides/2009/02/iphone-development-accessing-uicolor-components.ars)
+    // Convert to common RGBA colorspace if needed
     if (CGColorGetColorSpace(firstColor.CGColor) != CGColorGetColorSpace(secondColor.CGColor))
     {
-        NSLog(@"Attempted to interpolate between colors with different colorspaces: %@ and %@", firstColor, secondColor);
-        return nil;
+        firstColor = [UIColor colorConvertedToRGBA:firstColor];
+        secondColor = [UIColor colorConvertedToRGBA:secondColor];
     }
     
     // Grab color components
@@ -91,6 +90,34 @@
     
     [colors addObject:lastColor];
     return colors;
+}
+                          
++ (UIColor *)colorConvertedToRGBA:(UIColor *)colorToConvert;
+{
+    CGFloat red;
+    CGFloat green;
+    CGFloat blue;
+    CGFloat alpha;
+    
+    // Convert color to RGBA with a CGContext. UIColor's getRed:green:blue:alpha: doesn't work across color spaces. Adapted from http://stackoverflow.com/a/4700259
+
+    alpha = CGColorGetAlpha(colorToConvert.CGColor);
+    
+    CGColorRef opaqueColor = CGColorCreateCopyWithAlpha(colorToConvert.CGColor, 1.0f);
+    CGColorSpaceRef rgbColorSpace = CGColorSpaceCreateDeviceRGB();
+    unsigned char resultingPixel[CGColorSpaceGetNumberOfComponents(rgbColorSpace)];
+    CGContextRef context = CGBitmapContextCreate(&resultingPixel, 1, 1, 8, 4, rgbColorSpace, kCGImageAlphaNoneSkipLast);
+    CGContextSetFillColorWithColor(context, opaqueColor);
+    CGColorRelease(opaqueColor);
+    CGContextFillRect(context, CGRectMake(0.f, 0.f, 1.f, 1.f));
+    CGContextRelease(context);
+    CGColorSpaceRelease(rgbColorSpace);
+    
+    red = resultingPixel[0] / 255.0f;
+    green = resultingPixel[1] / 255.0f;
+    blue = resultingPixel[2] / 255.0f;
+
+    return [UIColor colorWithRed:red green:green blue:blue alpha:alpha];
 }
 
 @end
